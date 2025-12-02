22591af8bbe82ab548034e05a26caf96a7ec65f1
toilatinhs <toilatinhs@toilatinhss-MacBook-Air.local>
2025-12-02 14:16:31 +0700

    Fix base path for GitHub Pages repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: '/toilatinhs.design/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
